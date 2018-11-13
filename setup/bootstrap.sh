73e94c85bc596e6bd04ec8ed73a19b9638d165c5
Matthew Langlois <fletchto99@github.com>
2018-11-13 15:22:12 -0700

    Link source folder instead

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -10,14 +10,14 @@ fi
 
 # Check to ensure the /config volume is mounted
 if [ ! -d "/config" ]; then
-  echo "Please mount the _config.yml, _posts/, themes/ and public/ to the /config directory"
+  echo "Please mount the _config.yml, source/, themes/ and public/ to the /config directory"
   exit 1
 fi
 
 # Ensure the required directories exist
-mkdir -p /config/_posts
 mkdir -p /config/themes
 mkdir -p /config/public
+mkdir -p /config/source
 
 # Create a sample config
 cp -n /setup/_config.yml /config/_config.yml
@@ -29,13 +29,13 @@ fi
 
 # Remove some defaults setup by hexo
 rm /blog/_config.yml
-rm -r /blog/source/_posts
+rm -r /blog/source
 rm -r /blog/themes
 rm -r /blog/public
 
 # Link all the things to make hexo work
 ln -s /config/_config.yml /blog/_config.yml
-ln -s /config/_posts /blog/source/
+ln -s /config/source /blog/
 ln -s /config/themes /blog/
 ln -s /config/public /blog/
 
